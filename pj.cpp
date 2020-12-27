9cbbdee76433c53d06a110134da0fe98d3a8c165
tranvanson <son.tv1509@gmail.com>
2020-12-27 18:15:37 +0700

    Hoan thanh phep cong

---
+++ pj.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void tinhTong(int a,int b){
+        cout<<"Tong cua a va b la :"<<a+b;
+    }
+int main()
+{
+    int a,b;
+    a=10;b=12;
+    
+    
+    tinhTong(a,b);
+    return 0;
+} 
